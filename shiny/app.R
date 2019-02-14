99fb52d49d907dd0e0083c6fea5daedf2d562229
Michael Jones <mr.jones.olmedo@gmail.com>
2019-02-14 15:23:45 -0800

    * Add tooltips to UI inputs * Add sampling timestep UI input * Plot to use treatment A, B, etc, instead of treatment 1, 2, etc

--- shiny/app.R
+++ shiny/app.R
@@ -11,6 +11,7 @@
 # Organization: HD2i @ INGH @ MSSM
 
 library(shiny)
+library(shinyBS)
 library(ggplot2)
 source('../n1-simulator.R')
 source('utils.R')
@@ -53,56 +54,99 @@ ui <- function(request) {
         sliderInput("n_treatments", "Number of treatments:", value=2, min=1, max=5),
         hr(),
         h4("Treatment A"),
-        numericInput("effect_1", "Effect Size:", value=-40),
-        numericInput("tc_in_1", "Run In:", min=1, value=3),
-        numericInput("tc_out_1", "Wash Out:", min=1, value=2),
+        numericInput("effect_1", "Effect size:", value=-40),
+        numericInput("tc_in_1", "Run-in:", min=1, value=3),
+        numericInput("tc_out_1", "Wash-out:", min=1, value=2),
+        bsTooltip("effect_1", "The effect size is the asymptotic magnitude change from baseline that a person would experience in the long run, if they continued this treatment",
+                  placement = "right", trigger = "hover", options=list(container="body")),
+        bsTooltip("tc_out_1", "The \"wash-out\" (or \"carryover\") time constant is a measure of how long it takes for a treatment to stop working once it is discontinued",
+                  placement = "right", trigger = "hover", options=list(container="body")),
+        bsTooltip("tc_in_1", "The \"run-in\" (or \"wash-in\") time constant is a measure of how long it takes for a treatment to ramp up to its full effect",
+                  placement = "right", trigger = "hover", options=list(container="body")),
         conditionalPanel(
          condition = "input.n_treatments >= 2",
          hr(),
          h4("Treatment B"),
-         numericInput("effect_2", "Treatment B Effect Size:", value=-30),
-         numericInput("tc_in_2", "Treatment B Run In:", min=1, value=2),
-         numericInput("tc_out_2", "Treatment B Wash Out:", min=1, value=3)
+         numericInput("effect_2", "Effect size:", value=-30),
+         numericInput("tc_in_2", "Run-in:", min=1, value=2),
+         numericInput("tc_out_2", "Wash-out:", min=1, value=3),
+         bsTooltip("effect_2", "The effect size is the asymptotic magnitude change from baseline that a person would experience in the long run, if they continued this treatment",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_out_2", "The \"wash-out\" (or \"carryover\") time constant is a measure of how long it takes for a treatment to stop working once it is discontinued",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_in_2", "The \"run-in\" (or \"wash-in\") time constant is a measure of how long it takes for a treatment to ramp up to its full effect",
+                   placement = "right", trigger = "hover", options=list(container="body"))
         ),
         conditionalPanel(
          condition = "input.n_treatments >= 3",
          hr(),
          h4("Treatment C"),
-         numericInput("effect_3", "Treatment C Effect Size:", value=-40),
-         numericInput("tc_in_3", "Treatment C Run In:", min=1, value=3),
-         numericInput("tc_out_3", "Treatment C Wash Out:", min=1, value=2)
+         numericInput("effect_3", "Effect size:", value=-40),
+         numericInput("tc_in_3", "Run-in:", min=1, value=3),
+         numericInput("tc_out_3", "Wash-out:", min=1, value=2),
+         bsTooltip("effect_3", "The effect size is the asymptotic magnitude change from baseline that a person would experience in the long run, if they continued this treatment",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_out_3", "The \"wash-out\" (or \"carryover\") time constant is a measure of how long it takes for a treatment to stop working once it is discontinued",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_in_3", "The \"run-in\" (or \"wash-in\") time constant is a measure of how long it takes for a treatment to ramp up to its full effect",
+                   placement = "right", trigger = "hover", options=list(container="body"))
         ),
         conditionalPanel(
          condition = "input.n_treatments >= 4",
          hr(),
          h4("Treatment D"),
-         numericInput("effect_4", "Treatment D Effect Size:", value=-40),
-         numericInput("tc_in_4", "Treatment D Run In:", min=1, value=3),
-         numericInput("tc_out_4", "Treatment D Wash Out:", min=1, value=2)
+         numericInput("effect_4", "Effect size:", value=-40),
+         numericInput("tc_in_4", "Run-in:", min=1, value=3),
+         numericInput("tc_out_4", "Wash-out:", min=1, value=2),
+         bsTooltip("effect_4", "The effect size is the asymptotic magnitude change from baseline that a person would experience in the long run, if they continued this treatment",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_out_4", "The \"wash-out\" (or \"carryover\") time constant is a measure of how long it takes for a treatment to stop working once it is discontinued",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_in_4", "The \"run-in\" (or \"wash-in\") time constant is a measure of how long it takes for a treatment to ramp up to its full effect",
+                   placement = "right", trigger = "hover", options=list(container="body"))
         ),
         conditionalPanel(
          condition = "input.n_treatments >= 5",
          hr(),
          h4("Treatment E"),
-         numericInput("effect_5", "Treatment E Effect Size:", value=-40),
-         numericInput("tc_in_5", "Treatment E Run In:", min=1, value=3),
-         numericInput("tc_out_5", "Treatment E Wash Out:", min=1, value=2)
+         numericInput("effect_5", "Effect size:", value=-40),
+         numericInput("tc_in_5", "Run-in:", min=1, value=3),
+         numericInput("tc_out_5", "Wash-out:", min=1, value=2),
+         bsTooltip("effect_5", "The effect size is the asymptotic magnitude change from baseline that a person would experience in the long run, if they continued this treatment",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_out_5", "The \"wash-out\" (or \"carryover\") time constant is a measure of how long it takes for a treatment to stop working once it is discontinued",
+                   placement = "right", trigger = "hover", options=list(container="body")),
+         bsTooltip("tc_in_5", "The \"run-in\" (or \"wash-in\") time constant is a measure of how long it takes for a treatment to ramp up to its full effect",
+                   placement = "right", trigger = "hover", options=list(container="body"))
         )
       ),
       column(4,
         h3("Study Design"),
         sliderInput("n_blocks", "Number of treatment blocks:", value=2, min=1, max=10),
         uiOutput("treatmentScheduleOptions"),
-        numericInput("treatment_period", "Treatment Period:", value=30),
+        numericInput("treatment_period", "Treatment period:", value=30),
+        numericInput("sampling_timestep", "Samping timestep:", min=0.1, value=1, step=0.1),
+        bsTooltip("treatment_schedule", "Ordering of treatment periods",
+                  placement = "right", trigger = "hover", options=list(container="body")),
+        bsTooltip("treatment_period", "Number of time intervals (e.g. days) for each treatment period",
+                  placement = "right", trigger = "hover", options=list(container="body")),
+        bsTooltip("sampling_timestep", "Time period between samples within a time interval",
+                  placement = "right", trigger = "hover", options=list(container="body")),
         hr(),
-        h3("Statistical Parameters"),
-        numericInput("sd_baseline", "S.D. Baseline Drift", min=0, value=0.4, step=0.1),
-        numericInput("sd_outcome", "S.D. Process Noise", min=0, value=0.6, step=0.1),
-        numericInput("sd_obs", "S.D. Observation Noise", min=0, value=4, step = 0.1)
+        h3("Noise Parameters"),
+        numericInput("sd_baseline", "S.D. Baseline drift", min=0, value=0.4, step=0.1),
+        numericInput("sd_outcome", "S.D. Process noise", min=0, value=0.6, step=0.1),
+        numericInput("sd_obs", "S.D. Observation noise", min=0, value=4, step = 0.1),
+        bsTooltip("sd_baseline", "Baseline drift is a steady increase or decrease from baseline caused by long-term processes (e.g. long-term illness onset and recovery)",
+                  placement = "right", trigger = "hover", options=list(container="body")),
+        bsTooltip("sd_outcome", "Process noise consists of short-term fluctuations (e.g. changes in sleep and diet from day to day)",
+                  placement = "right", trigger = "hover", options=list(container="body")),
+        bsTooltip("sd_obs", "Observation noise is a function of the measurement instrument and not the underlying biological process (e.g. imprecision in a blood pressure cuff)",
+                  placement = "right", trigger = "hover", options=list(container="body"))
       ),
       column(4,
         h3("Randomization"),
-        numericInput("random_seed", "Random Seed:", 1),
+        numericInput("random_seed", "Random seed:", 1),
         actionButton("random_seed_generate", "Generate"),
         hr(),
         actionButton("reset_button", "Reset inputs to default"),
@@ -150,6 +194,7 @@ server <- function(input, output, session) {
     input$tc_out_4
     input$tc_out_5
     input$treatment_period
+    input$sampling_timestep
     input$sd_baseline
     input$sd_outcome
     input$sd_obs
@@ -201,7 +246,7 @@ server <- function(input, output, session) {
     n1_simulate(
       n_treatments = isolate(input$n_treatments), 
       n_blocks = isolate(input$n_blocks),
-      baseline_initial = 160,
+      baseline_initial = 0,
       effect_size = effect_sizes(),
       tc_in = tc_ins(),
       tc_out = tc_outs(),
@@ -210,7 +255,7 @@ server <- function(input, output, session) {
       sd_outcome = input$sd_outcome,
       sd_obs = input$sd_obs,
       treatment_period = input$treatment_period,
-      sampling_timestep = 1.0,
+      sampling_timestep = input$sampling_timestep,
       noise_timestep = 0.01,
       treatment_order = treatment_order_str_to_mat(isolate(input$n_blocks),input$treatment_schedule),
       random_seed = input$random_seed,
@@ -224,7 +269,7 @@ server <- function(input, output, session) {
   
   ## Render elements
   output$treatmentScheduleOptions <- renderUI({
-    selectInput("treatment_schedule", "Treatment Schedule:", treatment_options())
+    selectInput("treatment_schedule", "Treatment schedule:", treatment_options())
   })
   
   output$outcomePlot <- renderPlot({
@@ -234,7 +279,8 @@ server <- function(input, output, session) {
     timeseries <- n1sim_result()$timeseries
     plot <- ggplot(data = timeseries, aes(x = t)) 
     plot <- plot + geom_line(aes(y = outcome, alpha="underlying"), colour="#555555", size=0.75)
-    plot <- plot + geom_point(aes(y = outcome_obs, alpha="observed", colour=factor(treatment), shape=factor(treatment), fill=factor(treatment)))
+    tmtfactor <- LETTERS[factor(timeseries$treatment)]
+    plot <- plot + geom_point(aes(y = outcome_obs, alpha="observed", colour=tmtfactor, shape=tmtfactor, fill=tmtfactor))
     
     isolate({
       grid_x_major = seq(0, input$treatment_period*input$n_blocks*input$n_treatments, input$treatment_period)
@@ -242,14 +288,14 @@ server <- function(input, output, session) {
     })
     plot <- plot + scale_x_continuous(minor_breaks = grid_x_minor, breaks = grid_x_major)
     plot <- plot + ggtitle("Underlying and observed outcome") + theme(plot.title = element_text(hjust=0.5, face="bold"))
-    plot <- plot + xlab("Day in study") + ylab("Outcome")
+    plot <- plot + xlab("Time interval in study") + ylab("Outcome")
     plot <- plot + theme(text = element_text(size=14))
     plot <- plot + scale_colour_manual(name="Treatment",
-                                       values=c("1"="#009E73","2"="#D55E00","3"="#0072B2","4"="#CC79A7","5"="#E69F00"))
+                                       values=c("A"="#009E73","B"="#D55E00","C"="#0072B2","D"="#CC79A7","E"="#E69F00"))
     plot <- plot + scale_fill_manual(name="Treatment",
-                                     values=c("1"="#009E73","2"="#D55E00","3"="#0072B2","4"="#CC79A7","5"="#E69F00"))
+                                     values=c("A"="#009E73","B"="#D55E00","C"="#0072B2","D"="#CC79A7","E"="#E69F00"))
     plot <- plot + scale_shape_manual(name="Treatment",
-                                      values=c("1"=21,"2"=22,"3"=23,"4"=24,"5"=25))
+                                      values=c("A"=21,"B"=22,"C"=23,"D"=24,"E"=25))
     plot <- plot + scale_alpha_manual(name="Outcome",
                                       values=c("observed"=1,"underlying"=1),
                                       guide=guide_legend(override.aes = list(linetype=c("blank","solid"),shape=c(16,NA))))
@@ -295,6 +341,7 @@ server <- function(input, output, session) {
     updateNumericInput(session, "tc_out_5", value = 2)
 
     updateSliderInput(session, "treatment_period", value = 30)
+    updateNumericInput(session, "sampling_timestep", value = 1)
     
     updateNumericInput(session, "sd_baseline", value = 0.4)
     updateNumericInput(session, "sd_outcome", value = 0.6)
